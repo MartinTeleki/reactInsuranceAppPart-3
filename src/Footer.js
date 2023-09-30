958777a341c35e8e9fbf5286ad2a2e45ef6ff8be
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-30 21:02:37 +0200

    Add files via upload
    comments were added.

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./index.css";
+//import "./index.css";
+import "./footer.css";
 
 export function Footer() {
   return (
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./index.css";
-
+import "./navbar.css";
 export function NavBar({
   toggleMenu,
   changePage,
